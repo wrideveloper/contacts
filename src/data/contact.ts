3356774973f9a826075e0d77eed5b6b92d442c98
elianiva <dicha.arkana03@gmail.com>
2024-06-17 09:07:33 +0700

    feat: our own shor.by alternative

---
+++ src/data/contact.ts
@@ -0,0 +1,12 @@
+export const CONTACTS = [
+    { key: 'Email', value: 'mailto:contact@wridev.id', full: false },
+    { key: 'WhatsApp', value: import.meta.env.VITE_WHATSAPP_NUMBER, full: false },
+    { key: 'Github', value: 'https://github.com/wrideveloper', full: false },
+    { key: 'Website', value: 'https://wridev.id', full: false },
+    { key: 'LinkedIn', value: 'https://www.linkedin.com/in/wripolinema/', full: true },
+    { key: 'Roadmap', value: 'https://roadmap.wridev.id', full: true },
+    { key: 'Shortener', value: 'https://s.wridev.id', full: true },
+    { key: 'Medium', value: 'https://medium.com/@wripolinema', full: true },
+    { key: 'Instagram', value: 'https://www.instagram.com/wri_polinema/', full: false },
+    { key: 'YouTube', value: 'https://www.youtube.com/@workshoprisetinformatika', full: false },
+];
